f1ab92bb034e276f3755cf2fe533ef30f1321cf4
nivedhanr <nivedharenahan@gmail.com>
2022-12-08 03:51:50 +0530

    edited

--- src/app/userreg/userreg.component.ts
+++ src/app/userreg/userreg.component.ts
@@ -18,5 +18,11 @@ export class UserregComponent {
   {
     let data:any={"name":this.name,"address":this.address,"email":this.email,"phn":this.phn,"username":this.username,"password":this.password,"confirmpass":this.confirmpass}
     console.log(data)
+    if (this.password==this.confirmpass) {
+      alert("successfully registered")
+      
+    } else {
+      alert("check password and confirm password")
+    }
   }
 }
